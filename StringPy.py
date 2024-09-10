a5940226bb0cd663463c04424441a79a689ee93b
MujtabaFaiz <ansarimujtaba16@gmail.com>
2024-09-10 02:03:48 +0000

    string commit

--- StringPy.py
+++ StringPy.py
@@ -1,3 +1,60 @@
-#Strings
-Str = 'Hrllo Word'
-print(str)
+#==========================================Strings
+str = 'Hello Word'
+#print(str)
+
+#====================================== Quotes Inside Quotes
+# print("It's alright")
+# print("He is called 'Johnny'")
+# print('He is called "Johnny"')
+
+#==================================== Multiline Strings
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua"""
+#print(a)   can also use single ''''''
+
+#==================================Strings are Arrays
+a = 'Hello word'
+#print(a[1])
+
+#====================================loop with string
+#for x in a:  when commnet under the loop statment getting error after writeb new line code
+    #print(x)
+
+#================================ String Length
+st = 'string len'
+#print(len(st) )
+
+#===============================String Length
+
+text = "The best things in life are free!"
+#if "free" in text:
+    #print("Yes")
+#print("life" in text)
+
+#==================================Check if NOT
+text = "The best things in life are free!"
+# if "free" not in text:
+#     print("Yes")
+# print("life" not in text)
+
+#==================================Slicing string 
+b = "Hello, World!"
+#print(b[2:6])
+ 
+#=================================Slice From the Start
+b = "Hello, World!"
+#print(b[:5])  #Hello
+
+#================================Slice To the End
+b = "Hello, World!"
+#print(b[3:])  #lo, World!
+
+#===================================Negative Indexing
+
+b = "Hello, World!"
+#print(b[-3:-2])  #lo, World!
+
+#======================================Modify Strings
